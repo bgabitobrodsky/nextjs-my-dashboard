1c82e1f812b8b397dddb5f1995ac09f069a6070f
bgabitobrodsky <bernardogabito@gmail.com>
2024-03-14 13:04:34 -0300

    dynamic metadata

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,13 +1,41 @@
+import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
-    params: {id: string};
+	params: { id: string };
 }
 
-export default function PokemonPage({params}:Props) {
-    
+export async function generateMetadata({params}:Props):Promise<Metadata>{
+
+    const {id, name} = await getPokemon(params.id);
+
+    return {
+        title: `#${id} - ${name}`,
+        description:`Página del pokemon ${name}`
+    }
+}
+
+const getPokemon = async (id: string): Promise<Pokemon> => {
+	const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+		cache: "force-cache",
+        //next:{revalidate: 60*60*30*6}
+	}).then(res => res.json());
+
+    console.log('Se cargó: ' + pokemon.name);
+
+    return pokemon;
+};
+
+export default async function PokemonPage({ params }: Props) {
+
+    const pokemon = await getPokemon(params.id);
+
 	return (
 		<div>
 			<h1>Hello PokemonPage {params.id}</h1>
+            <div>
+                {pokemon.name}
+            </div>
 		</div>
 	);
 }
