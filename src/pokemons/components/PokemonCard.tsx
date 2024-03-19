4bd24c0479d9c09ccb259c995fb5235e659a0280
bgabitobrodsky <bernardogabito@gmail.com>
2024-03-19 08:10:14 -0300

    static site generation

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -27,14 +27,14 @@ export const PokemonCard = ({pokemon}:Props) => {
 						{name}
 					</p>
 					<div className="mt-5">
-						<a className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
+						<Link href={`/dashboard/pokemons/${name}`} className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
 							Más información
-						</a>
+						</Link>
 					</div>
 				</div>
 				<div className="border-b">
 					<Link
-						href={`/dashboard/pokemon/${id}`}
+						href={`/dashboard/pokemons/${name}`}
 						className="px-4 py-2 hover:bg-gray-100 flex items-center"
 					>
 						<div className="text-red-600">
